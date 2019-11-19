7bc309f24010c94af9722c4bd6624a6e07b056f2
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-19 14:47:25 -0600

    initalized database and set up seeds

--- data/migrations/20191119141427_create_projects.js
+++ data/migrations/20191119141427_create_projects.js
@@ -1,8 +1,17 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments();
+
+        tbl.string('name', 255).notNullable().unique();
+        tbl.string('description', 128).notNullable();
+        tbl.string('img');
+        tbl.string('link').notNullable();
+        tbl.string('deployed');
+        tbl.string('role');
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('projects');
 };
---
+++ data/seeds/001_projects.js
@@ -0,0 +1,87 @@
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('projects').del()
+    .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('projects').insert([
+        {
+          name: "Review It Yourself",
+          description: 'Review It Yourself will target people who want to get up and get productive and learn different skills to do projects themselves. There will be a rating system on which one is better. This will lead to the tutorials that are accurate and precise to be on top.',
+          img: '',
+          link: 'https://github.com/labs13-how-to',
+          deployed: 'https://how-tutor.netlify.com/',
+          role: 'Wed Developer'
+        },
+        {
+          name: "Mympy Dreams",
+          description: 'Mympy is on a mission to close the poverty gap in VR technology by creating a low barrier entry into the field of VR development for low-income individuals. Mympy Dreams is a marketplace where individuals can create a profile and post their project and needs and find funding from the Mympy Dreams community.',
+          img: '',
+          link: 'https://github.com/mympy-dreamers',
+          deployed: 'https://mympydreamers.netlify.com/',
+          role: 'Team Lead/Web Developer'
+        },
+        {
+          name: "Luncher App",
+          description: 'There are kids today in this country who go without student lunches. This app allows schools to broadcast the needs of their students by declaring an amount of donations that they would need to be fullfilled in order to provide lunches for those that go without.',
+          img: '',
+          link: 'https://github.com/luncher-team/LA-Backend-Nick-Stricker',
+          deployed: 'https://luncherappltrii.netlify.com/',
+          role: 'Wed Developer'
+        },
+        {
+          name: "Jango Rango Dungeon",
+          description: 'This project randomly Generates a map of 100 or more rooms. I used my knowledege of serach algorithms and data structures to generate a dungeon and be able to traverse it. We used django to send information of the dungeon to the frontend.',
+          img: '',
+          link: 'https://github.com/CS-multiplayer',
+          deployed: 'https://jango-rango-dungeon.netlify.com/',
+          role: 'Wed Developer'
+        },
+        {
+          name: "Lambda Notes",
+          description: 'This project allowed me to showcase my progress when i frist learned react. With this app you can view your list of notes as well as create, edit and delete any particular note.',
+          img: '',
+          link: 'https://github.com/NickStrick/front-end-project-week',
+          deployed: 'https://nstrick-lambda-notes.netlify.com/',
+          role: 'Wed Developer'
+        },
+        {
+          name: "Smith and Jones Architects",
+          description: 'My first project after starting lambda school showcases what i could build after 1 month of programming in javascript, html, and css.',
+          img: '',
+          link: 'https://github.com/NickStrick/User-Interface-Project-Week',
+          role: 'Wed Developer',
+        },
+
+        {
+          name: "CS build week 2",
+          description: 'In this project i tested my algorithm skills and traversed a 500 room dungeon, followed clues and mined a blockchain coin. I used my knowledge of search algorithms and graphs in order to mine a coin in the right location in the graph.',
+          img: '',
+          link: 'https://github.com/TBN-CS-build-week-2/CS-Build-Week-2',
+          role: 'Wed Developer'
+        },
+        {
+          name: "Stash.It",
+          description: 'This project allowed me to showcase my progress when i frist learned react. With this app you can view your list of notes as well as create, edit and delete any particular note.',
+          img: '',
+          link: 'https://github.com/tabless-thursday-4-15-2019',
+          role: 'Team Lead'
+        },
+        {
+          name: "Design Your Life",
+          description: 'In this project i tested my algorithm skills and traversed a 500 room dungeon, followed clues and mined a blockchain coin. I used my knowledge of search algorithms and graphs in order to mine a coin in the right location in the graph.',
+          img: '',
+          link: 'https://github.com/designyourlife-bw-3-11',
+          role: 'Team Lead'
+        },
+        {
+          name: "devdesk-queue",
+          description: 'My first project after starting lambda school showcases what i could build after 1 month of programming in javascript, html, and css.',
+          img: '',
+          link: 'https://github.com/devdesk-queue',
+          role: 'Team Lead',
+        },
+      ]);
+    });
+};
