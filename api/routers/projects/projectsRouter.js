a08a05606d3d16464016c6966593b1b75d90ce47
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-19 14:29:57 -0600

    added projects model and router

---
+++ api/routers/projects/projectsRouter.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
+
+const db = require('../../data/helpers/schoolModel.js');
+
+router.route('/')
+    .get(get)
+    .post(add)
+    ;
+
+router.route('/:id')
+    .get(getId)
+    .delete(remove)
+    .put(update)
+    ;
+
+
+// Functions
+function get(req, res) {
+    db.get()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => res.status(500).json({ msg: 'cant find table', err }));
+}
