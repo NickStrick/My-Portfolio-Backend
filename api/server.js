93cc831c879dba87fecca154823ce7f5ecce8d89
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-19 14:29:44 -0600

    added inital server file to api

---
+++ api/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+//routes
+server.get('/', (req, res) => {
+    res.send('Api Working');
+})
+
+
+module.exports = server;
