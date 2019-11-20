12968b065acd73b54f7e5a80a0ad9d4ddcc2b935
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-11-20 17:21:37 -0600

    added first test to server.js

--- api/routers/projects/projectsRouter.spec.js
+++ api/routers/projects/projectsRouter.spec.js
@@ -7,3 +7,23 @@ afterEach(async () => {
     await db('projects').truncate()
 });
 
+describe('server.js', () => {
+    describe('GET /projects endpoint', () => {
+        it('should respond with status code 200 OK', async () => {
+            let response = await request(server).get('/projects');
+
+            expect(response.status).toBe(200);
+        })
+        it('should respond with JSON', async () => {
+            let response = await request(server).get('/projects');
+
+            expect(response.type).toMatch(/json/i);
+        })
+        it('should respond with array', async () => {
+            const expected = [];
+            let response = await request(server).get('/projects');
+
+            expect(response.body).toMatchObject(expected);
+        })
+    })
+});
