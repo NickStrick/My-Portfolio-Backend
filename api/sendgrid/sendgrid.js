18f4cf2c22d4cce16c9cc62c391ab8019bd8a73b
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-11 18:16:44 -0600

    updated sendgrid

--- api/sendgrid/sendgrid.js
+++ api/sendgrid/sendgrid.js
@@ -5,19 +5,6 @@ const db = require('../../data/dbConfig');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// model functions
-
-const fromEmailDB = (id) => {
-    return db('users')
-        .select('users.email as fromEmail')
-        .where('users.id', id)
-}
-
-const fromUsernameDB = (id) => {
-    return db('users')
-        .select('users.username')
-        .where('users.id', id)
-}
 
 // Route functions ===========================
 const router = require('express').Router();
