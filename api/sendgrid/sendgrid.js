25f9a19cc9a90f1ef363c9a68a93357d1a4beaf0
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-12-11 18:13:37 -0600

    added route functions to sendgrid

--- api/sendgrid/sendgrid.js
+++ api/sendgrid/sendgrid.js
@@ -18,3 +18,34 @@ const fromUsernameDB = (id) => {
         .select('users.username')
         .where('users.id', id)
 }
+
+// Route functions ===========================
+const router = require('express').Router();
+module.exports = router;
+
+router.post('/', sendEmail);
+
+async function sendEmail(req, res) {
+    const { msg } = req.body;
+    // let toEmail;
+    // let fromEmail;
+    // let fromUser;
+
+    // await fromEmailDB(dreamer_id).then(result => toEmail = (result[0].fromEmail)).catch(err => res.send({ msg: "Failed get by dreamer id", err }));
+    // await fromEmailDB(user_id).then(result => fromEmail = result[0].fromEmail).catch(err => res.send({ msg: "Failed get by user-donor id ", err }));
+    // await fromUsernameDB(user_id).then(result => fromUser = result[0].username).catch(err => res.send({ msg: "Failed to get user username ", err }));
+
+    try {
+        const msg = {
+            to: 'strickerdev@gmail.com',
+            from: 'mymphydreamers@gmail.com',
+            subject: 'Someone from your protfolio wants to contact you!',
+            text: `${msg} `,
+        };
+
+        sgMail.send(msg);
+        res.status(200).send("email sent!")
+    } catch (err) {
+        res.status(400).send({ err, msg: 'failed to send email' })
+    }
+}
